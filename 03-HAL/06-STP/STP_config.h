c612027e2bf22a8d422bdd3f1e12835ff9b4b82c
Karim Mahmoud <karim.mahmoud.0109@gmail.com>
2022-12-09 21:59:44 +0200

    Serial To Parallel 74HC595 IC Driver

---
+++ 03-HAL/06-STP/STP_config.h
@@ -0,0 +1,17 @@
+/*************************************************************************************************************************************/
+/*  Author      : Karim Mahmoud                                                                                                      */
+/*  Date        : 9 DEC 2022                                                                                                         */
+/*  Layer       : HAL                                                                                                                */
+/*  SWC         : STP                                                                                                                */
+/*  Version     : V01                                                                                                                */
+/*************************************************************************************************************************************/
+
+#ifndef STP_CONFIG_H
+#define STP_CONFIG_H
+
+#define STP_SD_PIN          GPIO_PORTA,GPIO_PIN0
+#define STP_STC_PIN         GPIO_PORTA,GPIO_PIN1
+#define STP_SHC_PIN         GPIO_PORTA,GPIO_PIN2
+
+
+#endif
---
+++ 03-HAL/06-STP/STP_program.c
@@ -0,0 +1,26 @@
+/*************************************************************************************************************************************/
+/*  Author      : Karim Mahmoud                                                                                                      */
+/*  Date        : 9 DEC 2022                                                                                                         */
+/*  Layer       : HAL                                                                                                                */
+/*  SWC         : STP                                                                                                                */
+/*  Version     : V01                                                                                                                */
+/*************************************************************************************************************************************/
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "GPIO_interface.h"
+
+#include "STP_interface.h"
+#include "STP_private.h"
+#include "STP_config.h"
+
+void HSTP_voidTransferData(u8 Copy_u8Data){
+    MGPIO_u8SetPinValue(STP_STC_PIN,GPIO_PIN_LOW);
+    for(u8 Local_u8Iterator=0;Local_u8Iterator<8;Local_u8Iterator++){
+        MGPIO_u8SetPinValue(STP_SHC_PIN,GPIO_PIN_LOW);
+        MGPIO_u8SetPinValue(STP_SD_PIN,GET_BIT(Copy_u8Data,(7-Local_u8Iterator)));
+        MGPIO_u8SetPinValue(STP_SHC_PIN,GPIO_PIN_HIGH);
+    }
+    MGPIO_u8SetPinValue(STP_STC_PIN,GPIO_PIN_HIGH);
+}
